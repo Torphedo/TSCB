bf590f8599814088987ccdf30a8b7683b8a5d913
Torphedo <73564623+Torphedo@users.noreply.github.com>
2022-09-04 20:12:54 -0400

    YAML writing & Area array

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <memory.h>
-#include <stdlib.h>
 #include <stdint.h>
-#include <malloc.h>
 
 #include "main.h"
+#include "ryml.h"
 
 int main()
 {
@@ -25,7 +24,7 @@ int main()
         SwapEndianUInt(&header.Unknown1);
         SwapEndianUInt(&header.BaseTableOffset);
         SwapEndianFloat(&header.WorldScale);
-        SwapEndianFloat(&header.TerrainMaxHeight);
+        SwapEndianFloat(&header.TerrainHeightScale);
         SwapEndianUInt(&header.MaterialInfoLength);
         SwapEndianUInt(&header.AreaArrayLength);
         SwapEndianFloat(&header.TileSize);
@@ -33,12 +32,15 @@ int main()
         printf("Unknown 1: %d\n", header.Unknown1);
         printf("Base File Table Offset (Relative): 0x%x\n", header.BaseTableOffset);
         printf("World Scale: %f\n", header.WorldScale);
-        printf("Terrain Max Height: %f\n", header.TerrainMaxHeight);
+        printf("Terrain Height Scale: %f\n", header.TerrainHeightScale);
         printf("Size of Material Info Array: %d\n", header.MaterialInfoLength);
         printf("Size of Area Array: %d\n", header.AreaArrayLength);
         printf("Tile Size: %f\n", header.TileSize);
         printf("\n\nMaterial Information Section\n\n");
     }
+
+    FILE* yamlout = fopen("MainField.yaml", "wb");
+    ryml_set_header_data(header);
 
 	// Material Information Section
 	unsigned int SectionSize;
@@ -47,25 +49,74 @@ int main()
 	printf("Section Size: %u\n", SectionSize);
 
 	// Lookup table containing relative offsets to every element of the material info array
-	unsigned int Lookup[header.MaterialInfoLength];
-    fread(&Lookup, sizeof(unsigned int), header.MaterialInfoLength, TSCB_file);
-    for (int i = 0; i < header.MaterialInfoLength; i++)
+	unsigned int MatLookup[header.MaterialInfoLength];
+    fread(&MatLookup, sizeof(unsigned int), header.MaterialInfoLength, TSCB_file);
     {
-        SwapEndianUInt(&Lookup[i]);
+    for (int i = 0; i < header.MaterialInfoLength; i++)
+        SwapEndianUInt(&MatLookup[i]);  // Fix endian-ness
     }
 
     // Texture coordinates and other metadata
-	struct MaterialInfoData MatInfo[header.MaterialInfoLength];
-	fread(&MatInfo, sizeof(MatInfo), 1, TSCB_file);
 	for (int i = 0; i < header.MaterialInfoLength; i++)
 	{
-		SwapEndianUInt(&MatInfo[i].index);
-        SwapEndianFloat(&MatInfo[i].Texture_V);
-        SwapEndianFloat(&MatInfo[i].Texture_U);
-        SwapEndianFloat(&MatInfo[i].Unknown1);
-        SwapEndianFloat(&MatInfo[i].Unknown2);
+        struct MaterialInfoData MatInfo;
+        fread(&MatInfo, sizeof(MatInfo), 1, TSCB_file);
+        SwapEndianUInt(&MatInfo.index);
+        SwapEndianFloat(&MatInfo.Texture_V);
+        SwapEndianFloat(&MatInfo.Texture_U);
+        SwapEndianFloat(&MatInfo.Unknown1);
+        SwapEndianFloat(&MatInfo.Unknown2);
+        ryml_set_matinfo_data(MatInfo, i);
     }
 
+    // Area Array
+
+    unsigned int AreaLookup[header.AreaArrayLength];
+    fread(&AreaLookup, sizeof(unsigned int), header.AreaArrayLength, TSCB_file);
+    for (int i = 0; i < header.AreaArrayLength; i++)
+    {
+        SwapEndianUInt(&AreaLookup[i]); // Fix endian-ness
+    }
+
+    struct AreaArrayData AreaArray[header.AreaArrayLength];
+    for (int i = 0; i < header.AreaArrayLength; i++)
+    {
+        fread(&AreaArray[i], sizeof(AreaArray), 1, TSCB_file);
+        SwapEndianFloat(&AreaArray[i].XPosition);
+        SwapEndianFloat(&AreaArray[i].YPosition);
+        SwapEndianFloat(&AreaArray[i].AreaSize);
+        SwapEndianFloat(&AreaArray[i].MinTerrainHeight);
+        SwapEndianFloat(&AreaArray[i].MaxTerrainHeight);
+        SwapEndianFloat(&AreaArray[i].MinWaterHeight);
+        SwapEndianFloat(&AreaArray[i].MaxWaterHeight);
+        SwapEndianUInt(&AreaArray[i].Unknown1);
+        SwapEndianUInt(&AreaArray[i].FileBaseOffset);
+        SwapEndianUInt(&AreaArray[i].Unknown2);
+        SwapEndianUInt(&AreaArray[i].Unknown3);
+        SwapEndianUInt(&AreaArray[i].ref_extra);
+
+        if (AreaArray[i].ref_extra != 0)
+        {
+            fread(&AreaArray[i].ExtraInfoLength, sizeof(unsigned int), 1, TSCB_file);
+            SwapEndianUInt(&AreaArray[i].ExtraInfoLength);
+            if (AreaArray[i].ExtraInfoLength == 8)
+            {
+                fread(&AreaArray[i].HeaderUnknown, sizeof(unsigned int), 1, TSCB_file);
+                SwapEndianUInt(&AreaArray[i].HeaderUnknown);
+            }
+            for (int j = -1; j < (AreaArray[i].ExtraInfoLength / 4); j++)
+            {
+                struct ExtraAreaArray ExtraInfo;
+                fread(&ExtraInfo, sizeof(unsigned int) * 4, 1, TSCB_file);
+                SwapEndianUInt(&ExtraInfo.ExtraUnknown1); // Always 3.
+                SwapEndianUInt(&ExtraInfo.ExtraUnknown2); // Setting this to 0 = Grass, 1 = Water
+                SwapEndianUInt(&ExtraInfo.ExtraUnknown3); // Always 1.
+                SwapEndianUInt(&ExtraInfo.ExtraUnknown4); // Always 0.
+            }
+        }
+    }
+
+    ryml_emit(yamlout);
 
 	fclose(TSCB_file);
 	return 0;
